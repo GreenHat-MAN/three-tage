e0881db569e6339966246cc6c35273f975cb3f5b
GreenHat-MAN <1915684653@qq.com>
2022-07-14 11:42:55 +0800

    配置所有文件

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+// 导入全局样式
+import '@/assets/index.scss'
+// 导入vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 在全局下使用
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
--- myapp/vue.config.js
+++ myapp/vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  lintOnSave: false, //去除eslint的警告
+
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
+
+  devServer: {  //开发的服务器配置 
+    host: "localhost", // 0.0.0.0
+    port: 7000,
+    // inline:true, 
+    hot: true,  // 自动刷新页面
+    open: true,  // 自动打开浏览器 
+  }
+
 })
